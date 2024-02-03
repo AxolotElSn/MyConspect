7371638bbcfdce8cb70c76e7cdb6e61c6243ca8c
Alex Orlov <axolotsn@gmail.com>
2024-02-03 15:21:33 +0400

    объекты и callback ф-ии

--- js/script.js
+++ js/script.js
@@ -381,6 +381,86 @@
 // const num = 12.2;
 // console.log(Math.round(num)); // 12 округление
 
-const test = "12.2px";
-console.log(parseInt(test)) // превращает в другую систему счисления
-console.log(parseFloat(test)); 12.2 // возвращает в 10ой системе счисления
+// const test = "12.2px";
+// console.log(parseInt(test)) // превращает в другую систему счисления
+// console.log(parseFloat(test)); 12.2 // возвращает в 10ой системе счисления
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//метод trim
+
+//(строка).trim
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//callback - ф-ии это ф-ия, которая должна выполнится после того когда другвя ф-ия завершила свое выполнение
+
+// function first(){
+//     setTimeout(function() {
+//         console.log(1);
+//     }, 500); 
+// }
+
+// function second(){
+//     console.log(2);
+// }
+
+// first();
+// second();
+
+// /////
+
+// function learnJS(lang, callback){
+//     console.log(`Я учу:${lang}`)
+//     callback();
+// }
+
+// learnJS('JavaScript', function(){
+//     console.log('I lox')
+// })
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Объекты, деструктуризация объектов
+
+const options = {
+    name: 'boroda',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() { // можно делать свои методы в объектах
+        console.log("Test");
+    }
+}
+
+options.makeTest();
+
+const {border, bg} = options.colors; // такая структура называется деструктуризацией (на будущее пригодится)
+console.log(border);
+
+console.log(options.name);
+
+delete options.name;
+
+console.log(options);
+
+for (let key in options) {
+    if(typeof(options[key]) === 'object'){
+        for (let i in options[key]){
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`)
+        }
+    } else {
+        console.log(`Свойство ${key} имеет значение ${options[key]}`)
+    }
+}
+console.log(options["colors"] ["border"]);
+
+
+let counter = 0; // типа счетчик (чтобы узнать сколько свойств в объекте)
+for(let key in options){
+    counter++;
+}
+console.log(counter);
+
+console.log(Object.keys(options).length) // ну или более простой вариант метод Object.keys()
+
--- js/tempCodeRunnerFile.js
+++ js/tempCodeRunnerFile.js
@@ -1,2 +1,17 @@
-const num = 12.2;
-console.log(Math.round(num));
+const options = {
+    name: 'boroda',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() { // можно делать свои методы в объектах
+        console.log("Test");
+    }
+}
+
+options.makeTest();
+
+const {border, bg} = options.colors; // такая структура называется деструктуризацией (на будущее пригодится)
+console.log(border);
