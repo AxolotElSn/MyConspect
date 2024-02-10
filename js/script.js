fa0a321e4f027ab16660c0507136e4e0e3cecfda
Alex Orlov <axolotsn@gmail.com>
2024-02-10 21:01:35 +0400

    основы ООП + практика что-то написал сам, но опирался на готовый вариант

--- js/script.js
+++ js/script.js
@@ -870,48 +870,48 @@
 
 // - функция должна продолжать работать, даже если изменяется количество магазинов, высота, бюджет или подставляется вообще другой объект
 
-const shoppingMallData = {
-    shops: [
-        {
-            width: 10,
-            length: 5
-        },
-        {
-            width: 15,
-            length: 7
-        },
-        {
-            width: 20,
-            length: 5
-        },
-        {
-            width: 8,
-            length: 10
-        }
-    ],
-    height: 5,
-    moneyPer1m3: 30,
-    budget: 50000,
-    isBudgetEnough: function(data) {
-        let storeArea = 0;
-        let volume = 0;
+// const shoppingMallData = {
+//     shops: [
+//         {
+//             width: 10,
+//             length: 5
+//         },
+//         {
+//             width: 15,
+//             length: 7
+//         },
+//         {
+//             width: 20,
+//             length: 5
+//         },
+//         {
+//             width: 8,
+//             length: 10
+//         }
+//     ],
+//     height: 5,
+//     moneyPer1m3: 30,
+//     budget: 50000,
+//     isBudgetEnough: function(data) {
+//         let storeArea = 0;
+//         let volume = 0;
 
-        data.shops.forEach(shop => {
-            storeArea += shop.width * shop.length;
-        });
+//         data.shops.forEach(shop => {
+//             storeArea += shop.width * shop.length;
+//         });
 
-        volume = data.height * storeArea;
+//         volume = data.height * storeArea;
 
-        if(data.budget - (volume * data.moneyPer1m3) >= 0){
-            return 'Бюджета достаточно';
-        } else {
-            return 'Бюджета недостаточно';
-        }
-    }
-}
-shoppingMallData.isBudgetEnough(shoppingMallData);
-console.log(shoppingMallData.isBudgetEnough(shoppingMallData));
+//         if(data.budget - (volume * data.moneyPer1m3) >= 0){
+//             return 'Бюджета достаточно';
+//         } else {
+//             return 'Бюджета недостаточно';
+//         }
+//     }
+// }
+// shoppingMallData.isBudgetEnough(shoppingMallData);
+// console.log(shoppingMallData.isBudgetEnough(shoppingMallData));
 
 ///////////////////////////////////////////////////////////////////////////////////
-
+//
 
