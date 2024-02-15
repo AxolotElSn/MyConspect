1af046dfc9181ec11a17a1378845e6a98d833b3b
Alex Orlov <axolotsn@gmail.com>
2024-02-15 19:10:00 +0400

    динам. типизация и замыкание. + тест

--- js/script.js
+++ js/script.js
@@ -1029,19 +1029,26 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // это тест
 
-function foo(a,b) {
-    const [first] = a;
-    const {eng} = b;
+// NaN - number
+
+// function foo(a,b) {
+//     const [first] = a;
+//     const {eng} = b;
  
-    console.log(a);
+//     console.log(a);
 
-    return `${first} ${eng}`;
-}
+//     return `${first} ${eng}`;
+// }
 
-const result = foo(['Hello', 'Привет'], {ru: 'Мир', eng: 'World'})
-console.log(result);
+// const result = foo(['Hello', 'Привет'], {ru: 'Мир', eng: 'World'})
+// console.log(result);
 
 
-let time = '';
-5 > 3 || 2 ? time = 'Day' : time = 'Night' 
-console.log(time); // Day
+// let time = '';
+// 5 > 3 || 2 ? time = 'Day' : time = 'Night' 
+// console.log(time); // Day
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
