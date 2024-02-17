7b2b43713b6a1df82758594a10a9233bee47dbf8
Alex Orlov <axolotsn@gmail.com>
2024-02-17 13:48:16 +0400

    получение элементов со страницы и действия с ними

--- js/script.js
+++ js/script.js
@@ -1115,3 +1115,91 @@
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 //получение элементов со страницы
 
+// const box = document.getElementById('box'); // обращаемся к элементу по id
+
+// console.log(box); // <div class="box" id="box"></div>
+
+// // const btns = document.getElementsByTagName('button');
+// // console.log(btns); // получаем псевдомассив
+
+// // const btns = document.getElementsByTagName('button')[1]; // получаем 2ую кнопку
+// // console.log(btns);
+
+// const btns = document.getElementsByTagName('button'); // обращаемся к элементу по тегу
+// console.log(btns[1]); // получаем 2ую кнопку. просто другой вариант записи :3
+
+// const circles = document.getElementsByClassName('circle'); // обращаемся к элементу по классу
+// console.log(circles); // так же получаем html-коллекцию. Ну псевдомассив короче :D
+
+// const hearts = document.querySelectorAll('.heart'); // тут мы обращаемся по css селектор. А так же тут можно использовать forEach()
+// console.log(hearts);
+//  hearts.forEach(item => {
+//     console.log(item);
+//  });
+
+//  const oneHeart = document.querySelector('.heart'); // получает один элемент со страницы. Точнее первый элемент
+//  console.log(oneHeart);
+
+ ////////////////////////////////////////////////////////////////////////////////////////////////////
+ //действие с элементами
+
+// const box = document.getElementById('box'),
+//       btns = document.getElementsByTagName('button'),
+//       circles = document.getElementsByClassName('circle'),
+//       wrapper = document.querySelector('.wrapper'),
+//       hearts = wrapper.querySelectorAll('.heart'),
+//       oneHeart = wrapper.querySelector('.heart');
+
+// // box.style.backgroundColor = 'green';
+// // box.style.width = '500px';
+
+// box.style.cssText = 'background-Color: green; width: 500px'
+
+// btns[1].style.borderRadius = '100%';
+// circles[0].style.backgroundColor = 'red';
+
+
+// // for (let i = 0; i < hearts.length; i++){
+// //     hearts[i].style.backgroundColor = 'brown';
+// // }
+
+// hearts.forEach(item => {
+//     item.style.backgroundColor = 'brown';
+// });
+
+// const div = document.createElement('div'); // создание элемента. Но он существует только внутри файла js
+// // const text = document.createTextNode('Тут был я');
+
+// div.classList.add('black'); // добавляем класс 
+
+// wrapper.append(div); // тут уже элемент прописывается на странице благодаря методу append()
+// // document.querySelector('.wrapper').append(div); // так можно прописывать элементы внутри родителей
+// // wrapper.appendChild(div); // устаревшая запись
+
+// // wrapper.prepend(div); // этот метод вставляет в начало элемент
+
+// // hearts[0].before(div); до [] элемента
+// // hearts[0].after(div); // после [] элемента
+
+// // wrapper.insertBefore(div, hearts[1]); // тоже устаревшая запись. В скобках первый аргумент- то что вставляем, второй перед каким элементом вставляем
+
+// // circles[0].remove(); // удаление элемента
+// // wrapper.removeChild(hearts[1]); // тоже устаревшая запись
+
+// hearts[0].replaceWith(circles[0]); // заменили первое сердечко первым кружочком
+// // wrapper.replaceChild(circles[0], hearts[0]); // да еще одна устаревшая запись :D
+
+// // div.innerHTML = "<h1>Hello World</h1>"; // можно вставлять html - структуры, ну или просто текст
+
+// // div.textContent = "Hello"; // в этот метод html - структура не войдет. Только текст (это еще вопрос безопасности)
+
+
+// div.innerHTML = "<h1>Hello World</h1>";
+// //этот метод вставляет кусочек html - кода перед или после тегов
+// div.insertAdjacentHTML('beforebegin', '<h2>Hello1</h2>') //beforebegin вставляет html - код перед элементом
+// div.insertAdjacentHTML('afterbegin', '<h2>Hello2</h2>') // afterbegin вставляет html - код в начало 
+// div.insertAdjacentHTML('beforeend', '<h2>Hello3</h2>') // beforeend вставляет html - код преред концом
+// div.insertAdjacentHTML('afterend', '<h2>Hello4</h2>') // afterend вставляет html - код после 
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
