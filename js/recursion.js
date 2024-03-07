2bc6c0e2590b7dcb7ae0508cb845a2f7929c611b
Alex Orlov <axolotsn@gmail.com>
2024-03-07 23:12:46 +0400

    Рекурсия v2

--- js/recursion.js
+++ js/recursion.js
@@ -1,5 +1,7 @@
 // рекурсия ю-ху
 
+// 1
+
 function pow(x, n){ // задача на возведение в степень
     let res = 1;
 
@@ -11,9 +13,9 @@ function pow(x, n){ // задача на возведение в степень
 }
 
 function pow(x, n) { // тоже самое, только рекурсия
-    if (n === 1) {
-        return x;
-    } else {
+    if (n === 1) { // - база рекурсии
+        return x; // n - 1 это шаг рекурсии
+    } else { // глубина рекурсии это кол-во вложенных вызовов ф-ии
         return x * pow(x, n - 1); // при каждом вызове ф-ии уменьшаем n до того момента когда n станет равным 1.
         //Тогда каждая вызванная ф-ия возвращает 2 n - 1 раз. получается x в степени n - 1. По этому еще перед pow() стоит * 2 чтобы получилось число x в степени n
     } // т. е. ф-ия ввызывает саму себя до того момента пока не достигнит базового значения, в нашем случае это n = 1, а потом n - 1 раз умножит на n на x и еще раз на x
@@ -32,3 +34,60 @@ console.log(pow(2, 2)); //4
 console.log(pow(2, 3)); //8
 console.log(pow(2, 4)); //16
 
+// 2
+
+let students = {
+    js: [{
+        name: 'John',
+        progress: 100
+    }, {
+        name: 'Alex',
+        progress: 60
+    }],
+
+    html: {
+        basic: [{
+            name: 'Peter',
+            progress: 20
+        }, {
+        name: 'Ann',
+        progress: 18
+        }],
+
+        pro: [{
+            name: 'Sam',
+            progress: 10
+        }],
+    }
+};
+
+
+function getTotalProgressByIreration(data) {
+    let total = 0;
+    let students = 0;
+
+    for (let course of Object.values(data)){ // Object.values - это метод позволяющий получить массив со значениями в объекте (в нашем случае объекта students). В coursre записывается отжельный элемент массива
+        if (Array.isArray(course)) { // Array.isArray - метод возвращающий true, если объект является массивом, и соответственно false, если нет
+            students += course.length;
+            
+            for (let i = 0; i < course.length; i++){
+                total += course[i].progress; // i - получается номер массива. То есть course[0] - это первый массив, course[1] это второй массив
+            }
+        } else {
+            for (let subCourse of Object.values(course)){ // course получается менее вложенный объект
+             students += subCourse.length;
+
+                for (let i = 0; i < subCourse.length; i++){
+                    total += subCourse[i].progress;
+                }
+            }
+        }
+    }
+
+    return total / students;
+}
+
+console.log(getTotalProgressByIreration(students));
+
+
+// остановился на 25 минуте
--- js/tempCodeRunnerFile.js
+++ js/tempCodeRunnerFile.js
@@ -1,12 +1,52 @@
-function pow(x, n) { // тоже самое, только рекурсия
-    if (n === 1) {
-        return x;
-    } else {
-        return x * pow(x, n - 1); // при каждом вызове ф-ии уменьшаем n до того момента когда n станет равным 1. Тогда ф-ия начинает возвращаться обратно умножая x на 2
-    } // т. е. ф-ия ввызывает саму себя до того момента пока не достигнит базового значения, в нашем случае это n = 1, а потом n - 1 раз умножит на n на x и еще раз на x
+let students = {
+    js: [{
+        name: 'John',
+        progress: 100
+    }, {
+        name: 'Alex',
+        progress: 60
+    }],
+
+    html: {
+        basic: [{
+            name: 'Peter',
+            progress: 20
+        }, {
+        name: 'Ann',
+        progress: 18
+        }],
+
+        pro: [{
+            name: 'Sam',
+            progress: 10
+        }],
+    }
+};
+
+
+function getTotalProgressByIreration(data) {
+    let total = 0;
+    let students = 0;
+
+    for (let course of Object.values(data)){ // Object.values - это метод позволяющий получить массив со значениями в объекте (в нашем случае объекта students). В coursre записывается отжельный элемент массива
+        if (Array.isArray(course)) { // Array.isArray - метод возвращающий true, если объект является массивом, и соответственно false, если нет
+            students += course.length;
+            
+            for (let i = 0; i < course.length; i++){
+                total += course[i].progress; // i - получается номер массива. То есть course[0] - это первый массив, course[1] это второй массив
+            }
+        } else {
+            for (let subCourse of Object.values(course)){ // course получается менее вложенный объект
+             students += subCourse.length;
+
+                for (let i = 0; i < subCourse.length; i++){
+                    total += subCourse[i].progress;
+                }
+            }
+        }
+    }
+
+    return total / students;
 }
 
-console.log(pow(2, 1)); //2
-console.log(pow(2, 2)); //4
-console.log(pow(2, 3)); //8
-console.log(pow(2, 4)); //16
+console.log(getTotalProgressByIreration(students));
