31edab6c57baad1e3c38c1b7384faa9a4d032385
Alex Orlov <axolotsn@gmail.com>
2024-07-21 20:38:01 +0400

    rest оператор

--- conspect3/js/tempCodeRunnerFile.js
+++ conspect3/js/tempCodeRunnerFile.js
@@ -1,39 +1,7 @@
-'use strict';
-
-class Rectangle { // 1) тут концепция. Такой принцип ооп называется абстракция. То есть это когда мы отделяем концепцию от ее экземпляра
-    constructor(height, width) {
-        this.height = height;
-        this.width = width;
-    }
-
-    calcArea() {
-        return this.height * this.width;
-    }
+function calcOrDouble(number, basis = 2) { // ну а сейчас можно просто значение по умолчанию писать сразу
+    // basis = basis || 2; // так делали раньше. То есть если basis не задан, то подставится 2 по умолчанию
+    console.log(number * basis);
 }
 
-// 2) такой принцип ооп называется наследование
-class ColoredRectangleWithText extends Rectangle { // extends - наследование. То есть класс ColoredRectangleWithText унаследует все методы и конструкторы класса Rectangle (удобно)
-    constructor (height, width, text, bgColor) {
-        super(height, width); // ВАЖНО: super() должен быть на самом первом месте в конструкторе. Метод вызывающий супер конструктор родителя. Простыми словами вызывает тоже самое что было у конструктора родителя
-        this.text = text;
-        this.bgColor = bgColor;
-    }
 
-    showMyProops() {
-        console.log(`Текст: ${this.text}, цвет: ${this.bgColor}`);
-    }
-}
-
-// 2)
-const div = new ColoredRectangleWithText(25, 10, 'ahaha', 'red');
-div.showMyProops()
-console.log(div.calcArea());
-
-// 1) тут экземпляр
-const square = new Rectangle(10, 20); // в square теперь лежит объект со свойствами height и width и методом calcArea() (напоминание с прошлого урока)
-const long = new Rectangle(15, 100);
-
-console.log(square.calcArea()); // 200
-console.log(long.calcArea()); // 1500
-
-
+calcOrDouble(3);
