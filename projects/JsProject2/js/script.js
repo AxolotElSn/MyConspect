862b92f899befa8d36acf51e30cc3e593ef0ee7e
Alex Orlov <axolotsn@gmail.com>
2024-08-12 17:03:00 +0400

    slider

--- projects/JsProject2/js/script.js
+++ projects/JsProject2/js/script.js
@@ -373,4 +373,134 @@ window.addEventListener('DOMContentLoaded', () => {
     //     .then(data => data.json())
     //     .then(result => console.log(result));
 
+
+    // slider
+    // простой вариант
+
+    // const slides = document.querySelectorAll('.offer__slide'),
+    //       prev = document.querySelector('.offer__slider-prev'), // конопки вперед и
+    //       next = document.querySelector('.offer__slider-next'), // назад
+    //       total = document.querySelector('#total'), // сколько слайдов всего
+    //       current = document.querySelector('#current'); // на каком мы слайде сейчас
+    // let slideIndex = 1;
+
+    // showSlides(slideIndex); // для начала нужно записать начальное значене
+
+    // // ставим 0 в начале. Тут сколько слайдов всего
+    // if (slides.length < 10) { // не записываем в ф-ию потому что нам нужно просчитать кол-во слайдов один раз
+    //     total.textContent = `0${slides.length}`;
+    // } else {
+    //     total.textContent = slides.length;
+    // }
+
+    // function showSlides(n) {
+    //     if (n > slides.length) { // если дошли до крайнего слайда, то перемещаемся на первый
+    //         slideIndex = 1;
+    //     }
+
+    //     if (n < 1) { // наоборот
+    //         slideIndex = slides.length;
+    //     }
+
+    //     slides.forEach(item => item.style.display = 'none');
+
+    //     slides[slideIndex - 1].style.display = 'block'; // показываем нулевой слайд
+
+    //     // ставим 0 в начале. Тут на каком мы слайде сейчас
+    //     if (slides.length < 10) { // а это в ф-ии потому что нам нужно просчитывать текущий слайд при каждом нажатии на кнопку прокрутки
+    //         current.textContent = `0${slideIndex}`;
+    //     } else {
+    //         current.textContent = slideIndex;
+    //     }
+    // }
+
+    // function plusSlides(n) { // отвечает за переключение слайдов
+    //     showSlides(slideIndex += n)
+    // }
+
+    // prev.addEventListener('click', () => { // переключение назад
+    //     plusSlides(-1);
+    // });
+
+    // next.addEventListener('click', () => { // переключение вперед
+    //     plusSlides(1);
+    // });
+
+    // тот же слайдер, но более сложный вариант
+
+    const slides = document.querySelectorAll('.offer__slide'),
+          prev = document.querySelector('.offer__slider-prev'), // конопки назад и
+          next = document.querySelector('.offer__slider-next'), // вперед
+          total = document.querySelector('#total'), // сколько слайдов всего
+          current = document.querySelector('#current'), // на каком мы слайде сейчас
+          slidesWrapper = document.querySelector('.offer__slider-wrapper'), // общая обертка
+          slidesField = document.querySelector('.offer__slider-inner'), // обертка которая для прокрутки
+          width = window.getComputedStyle(slidesWrapper).width; // примененные стили от css. получаем ширину
+
+    let slideIndex = 1;
+    let offset = 0;
+
+    slidesField.style.width = 100 * slides.length + '%'; // устонавливаем ширтну блока. Умножаем кол-во слайдов на 100%. В нашем случае он будет занимать 400%
+    slidesField.style.display = "flex";
+	slidesField.style.transition = "0.5s all";
+
+    slidesWrapper.style.overflow = 'hidden'; // скрываем элементы которые не попадают в область видимости
+
+    slides.forEach(slide => { // делаем все слайды одной ширины
+        slide.style.width = width;
+    });
+
+    if (slides.length < 10) { // ставим 0 в начале числа если оно менише 10 в общем количестве слайдов
+        total.textContent = `0${slides.length}`; 
+        current.textContent = `0${slideIndex}`;
+    } else {
+        total.textContent = slides.length;
+        current.textContent = slideIndex;
+    }
+
+    next.addEventListener('click', () => {
+        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) { // slice() убирает px из ширины блока. Это для того чтоб мы работали с числом а не строкой. -1 как я понял нужен потому что у нас и так всегда 1 слайд отрисовывается, остается 3 прокручивать. А без минуса прокрутит 4 раза
+            offset = 0; // проверка на последний слайд. То есть если offset равен ширине когда в блоке последний слайд, мы offset присваиваем 0 - иначе говоря перемещаемся на первай слайд
+        } else {
+            offset += +width.slice(0, width.length - 2); // иначе при каждом нажатии на кнопку прибавляем offset ширину слайда
+        }
+
+        slidesField.style.transform = `translateX(-${offset}px)`; // перемещаем элемент по оси x. Минус используется потому что если элемент двигается влево то мы используем отрицательное значение, если вправо, то положительное. При нажатии на кнопки элементы прокручиваются справа на лево
+        
+        if(slideIndex == slides.length) {
+            slideIndex = 1;
+        } else {
+            slideIndex++;
+        }
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+    });
+
+    prev.addEventListener('click', () => {
+        if (offset == 0) { // Ну а тут наоборот. Если offset = 0, и мы нажимаем на кнопку, то offset становится равным той ширине при которой у нас отрисовывается последний слайд. То есть мы перемещаемся на последний слайд
+            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+        } else { // иначе отнимаем каждый раз ширину одного слайда
+            offset -= +width.slice(0, width.length - 2);
+        }
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
+    
+        if(slideIndex == 1) { 
+            slideIndex = slides.length;
+        } else {
+            slideIndex--;
+        }
+
+        if (slides.length < 10) { // ставим 0 в начале если число конкретного слайда меньше 10
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+    });
+    
+
 });
