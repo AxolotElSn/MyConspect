abb34b0c0ccecf5680f954fc8142dec9e5c6a8a9
Alex Orlov <axolotsn@gmail.com>
2024-08-12 18:44:42 +0400

    точки в слайдере

--- projects/JsProject2/js/script.js
+++ projects/JsProject2/js/script.js
@@ -429,6 +429,7 @@ window.addEventListener('DOMContentLoaded', () => {
     // тот же слайдер, но более сложный вариант
 
     const slides = document.querySelectorAll('.offer__slide'),
+          slider = document.querySelector('.offer__slider'), // для того чтоб можно было обратиться и сделать position relative (это весь блок слайда)
           prev = document.querySelector('.offer__slider-prev'), // конопки назад и
           next = document.querySelector('.offer__slider-next'), // вперед
           total = document.querySelector('#total'), // сколько слайдов всего
@@ -449,6 +450,49 @@ window.addEventListener('DOMContentLoaded', () => {
     slides.forEach(slide => { // делаем все слайды одной ширины
         slide.style.width = width;
     });
+
+    slider.style.position = 'relative';
+    const indicators = document.createElement('ol'), // большая обертка для наших точек (список)
+          dots = [];
+    indicators.classList.add('carousel-indicators');
+    indicators.style.cssText = `
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 15;
+        display: flex;
+        justify-content: center;
+        margin-right: 15%;
+        margin-left: 15%;
+        list-style: none;
+    `;
+    slider.append(indicators);
+
+    for (let i = 0; i < slides.length; i++) { // делаем точки для каждого слайда
+        const dot = document.createElement('li'); // делаем list item
+        dot.setAttribute('data-slide-to', i + 1); // делаем атрибуты точкам нумеруя начиная с 1
+        dot.style.cssText = `
+            box-sizing: content-box;
+            flex: 0 1 auto;
+            width: 30px;
+            height: 6px;
+            margin-right: 3px;
+            margin-left: 3px;
+            cursor: pointer;
+            background-color: #fff;
+            background-clip: padding-box;
+            border-top: 10px solid transparent;
+            border-bottom: 10px solid transparent;
+            opacity: .5;
+            transition: opacity .6s ease;
+        `;
+        if (i == 0) {
+            dot.style.opacity = 1; // делаем точку не прозрачной
+        }
+        indicators.append(dot);
+        dots.push(dot); // помещаем точку в массив
+    }
 
     if (slides.length < 10) { // ставим 0 в начале числа если оно менише 10 в общем количестве слайдов
         total.textContent = `0${slides.length}`; 
@@ -478,6 +522,9 @@ window.addEventListener('DOMContentLoaded', () => {
         } else {
             current.textContent = slideIndex;
         }
+
+        dots.forEach(dot => dot.style.opacity = '.5');
+        dots[slideIndex - 1].style.opacity = 1; // делаем у текущего слайда не прозрачную точку. (-1 потому что на странице у нас нумерация начинается с 1, а в js с 0)
     });
 
     prev.addEventListener('click', () => {
@@ -500,7 +547,30 @@ window.addEventListener('DOMContentLoaded', () => {
         } else {
             current.textContent = slideIndex;
         }
+
+        dots.forEach(dot => dot.style.opacity = '.5');
+        dots[slideIndex - 1].style.opacity = 1;
     });
+
+    dots.forEach(dot => {
+        dot.addEventListener('click', (e) => {
+            const slideTo = e.target.getAttribute('data-slide-to');
+
+            slideIndex = slideTo;
+            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+
+            slidesField.style.transform = `translateX(-${offset}px)`;
+
+            if (slides.length < 10) {
+                current.textContent = `0${slideIndex}`;
+            } else {
+                current.textContent = slideIndex;
+            }
+
+            dots.forEach(dot => dot.style.opacity = '.5');
+            dots[slideIndex - 1].style.opacity = 1;
+        })
+    })
     
 
 });
