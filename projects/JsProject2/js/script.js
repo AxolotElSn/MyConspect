152f71d0cd321b39556d03399a231263d2432e16
Alex Orlov <axolotsn@gmail.com>
2024-09-09 23:51:40 +0400

    Калькулятор v1.0

--- projects/JsProject2/js/script.js
+++ projects/JsProject2/js/script.js
@@ -577,6 +577,81 @@ window.addEventListener('DOMContentLoaded', () => {
             styleDots(dots, slideIndex);
         })
     })
+
+    // Calc
+
+    const result = document.querySelector('.calculating__result span');
+    let sex = 'female', 
+        height, weight, age, 
+        ratio = 1.375 // пол рост вес возраст коэф. атив.
+
+    function calcTotal() { // подсчет
+        if (!sex || !height || !weight || !age || !ratio) { // проверка на то что все поля заполнены 
+            result.textContent = '____';
+            return; // return нужен чтоб досрочно прервать нашу ф-ию
+        }
+
+        if (sex === 'female') { // формула в зависимости от пола
+            result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio) // Math.round - округление до ближайшего целого
+        } else {
+            result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio)
+        }
+
+
+    }
+
+    calcTotal();
+
+    function getStaticInformation(parentSelector, activeClass) { // чтоб зеленым выделялся
+        const elements = document.querySelectorAll(`${parentSelector} div`) // получаем все дивы внктри родителя (такая запись для того чтоб можно было ф-ию вызвать 2 рпзп для разных классов. То есть для блока с выбором рола и блока с выбором активности)
+
+        elements.forEach(elem => {
+            elem.addEventListener('click', (e) => { // получается мы накидываем событие родителя с нашими дивами (Делегирование событий)
+                if (e.target.getAttribute('data-ratio')) { // если атрибут присутствует у объекта события
+                    ratio = +e.target.getAttribute('data-ratio') // то сюда записываем ту активность которая стояла в этом атрибуте
+                } else { // ну а если нет, то значит мы кликаем на кнопну с выбором пола (в html - у нас дата атрибуты стоят на дневной активности, а id на выборе пола)
+                    sex = e.target.getAttribute('id');
+                }
     
+    
+                elements.forEach(elem => { // убираем у всех элементов активный класс
+                    elem.classList.remove(activeClass)
+                });
+                
+                // console.log(e.target) // напоминание что такое e.target - <div data-ratio="'1.55" id="medium" class="calculating__choose-item calculating__choose-item_active">Умеренная активность</div>
+                
+                e.target.classList.add(activeClass) // ну а тут добавляем активный класс на элемент на котрый мы кликнули
+    
+                calcTotal();
+            });
+        })
+
+    }
+
+    getStaticInformation('#gender', 'calculating__choose-item_active'); // вызов ф-ии для блока с полом
+    getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active'); //  вызов ф-ии для блока с дневной активностью
+
+    function getDynamicInformation(selector) { // обработка значений (рост, вес, возраст)
+        const input = document.querySelector(selector);
+
+        input.addEventListener('input', () => {
+            switch(input.getAttribute('id')) {
+                case 'height': // если вводим данные в блок роста
+                    height = +input.value // то записываем в рост то что написали в блок (input - наш блок, value - то что записано в блоке)
+                    break;
+                case 'weight':
+                    weight = +input.value
+                    break;
+                case 'age':
+                    age = +input.value
+                    break;
+            }
+            calcTotal();
+        });
+    }
+
+    getDynamicInformation('#height');
+    getDynamicInformation('#weight');
+    getDynamicInformation('#age');
 
 });
