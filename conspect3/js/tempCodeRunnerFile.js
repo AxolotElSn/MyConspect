107fdbb101500dfd25bf27567aa2ae3fe1f9b321
Alex <axolotsn@gmail.com>
2024-11-12 00:36:45 +0400

    расфасовал папочки

--- conspect3/js/tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-function calcOrDouble(number, basis = 2) { // ну а сейчас можно просто значение по умолчанию писать сразу
-    // basis = basis || 2; // так делали раньше. То есть если basis не задан, то подставится 2 по умолчанию
-    console.log(number * basis);
-}
-
-
-calcOrDouble(3);
