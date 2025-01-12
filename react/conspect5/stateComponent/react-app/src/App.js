a9132555565c55289e0ad4731114425b060287df
Alex Orlov <axolotsn@gmail.com>
2025-01-12 16:26:23 +0400

    дз

--- react/conspect5/stateComponent/react-app/src/App.js
+++ react/conspect5/stateComponent/react-app/src/App.js
@@ -45,3 +45,70 @@ function App() {
 }
 
 export default App;
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Дз
+// 1) Начальное значение счетчика должно передаваться через props
+// 2) INC и DEC увеличивают и уменьшают счетчик соответственно на 1. Без ограничений, но можете добавить границу в -50/50. По достижению границы ничего не происходит
+// 3) RND изменяет счетчик в случайное значение от -50 до 50. Конструкцию можете прогуглить за 20 секунд :) Не зависит от предыдущего состояния 
+// 4) RESET сбрасывает счетчик в 0 или в начальное значение из пропсов. Выберите один из вариантов
+
+// class App extends React.Component {
+// 	constructor(props) {
+// 	  super(props);
+// 	  this.state = {
+// 		counter: this.props.counter
+// 	  }
+// 	}
+	
+// 	incCounter = () => {
+// 	  if (this.state.counter < 50) {
+// 		  this.setState(state => ({
+// 		  counter: state.counter + 1
+// 		}))
+// 	  }
+// 	}
+	
+// 	decCounter = () => {
+// 	  if (this.state.counter > -50) {
+// 		  this.setState(state => ({
+// 		  counter: state.counter - 1
+// 		}))
+// 	  }
+// 	}
+	
+// 	rndCounter = () => {
+// 	  const max = 50,
+// 			min = -50
+// 	  this.setState({
+// 		counter: +(Math.random() * (max - min) + min).toFixed(0)
+// 	  })
+// 	}
+	
+// 	resetCounter = () => {
+// 	  this.setState ({
+// 		counter: this.props.counter
+// 	  })
+// 	}
+	
+  
+	
+// 	render() {
+// 	  const {counter} = this.state;
+	  
+// 	  return (
+// 		<div class="app">
+// 		  <div class="counter">{counter}</div>
+// 		  <div class="controls">
+// 			<button onClick={this.incCounter}>INC</button>
+// 			<button onClick={this.decCounter}>DEC</button>
+// 			<button onClick={this.rndCounter}>RND</button>
+// 			<button onClick={this.resetCounter}>RESET</button>
+// 		  </div>
+// 		</div>
+// 	  )
+// 	}
+//   }
+  
+//   ReactDOM.render(<App counter={0}/>, document.getElementById('app'));
