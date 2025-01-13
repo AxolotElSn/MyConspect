9c5afc089cdf4be305ad52ae913b4cb1100de1aa
Alex Orlov <axolotsn@gmail.com>
2025-01-13 20:01:56 +0400

    управляемые и неуправляемые компоненты

--- projects/ReactProjects/ReactProject1/react-app/src/components/employees-add-form/employees-add-form.js
+++ projects/ReactProjects/ReactProject1/react-app/src/components/employees-add-form/employees-add-form.js
@@ -1,22 +1,48 @@
+import {Component} from 'react'
+
 import './employees-add-form.css';
 
-const EmpolyeesAddForm = () => {
-    return (
-        <div className="app-add-form">
-            <h3>Добавьте нового сотрудника</h3>
-            <form className="add-form d-flex">
-                <input type="text"
-                    className="form-control new-post-label"
-                    placeholder="Как его зовут?"/>
-                <input type="number"
-                className="form-control new-post-label"
-                placeholder="З/П в $?"/>
+class EmpolyeesAddForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            salary: ''
+        }
+    }
 
-                <button type="submit"
-                        className="btn btn-outline-light">Добавить</button>
-            </form>
-        </div>
-    );
+    onValueChange = (e) => {
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
+
+    render() {
+        const {name, salary} = this.state;
+
+        return (
+            <div className="app-add-form">
+                <h3>Добавьте нового сотрудника</h3>
+                <form className="add-form d-flex">
+                    <input type="text" /* теперь то что мы вводим в эти инпуты контролируется именно реактом это называется управляемый элемент. Это дает нам что интерфейс будет мгновенно реагировать га все изменения. Потому что state связан с ui */
+                           className="form-control new-post-label"
+                           placeholder="Как его зовут?"
+                           name="name"
+                           value={name}
+                           onChange={this.onValueChange}/>
+                    <input type="number"
+                           className="form-control new-post-label"
+                           placeholder="З/П в $?"
+                           name="salary"
+                           value={salary}
+                           onChange={this.onValueChange}/>
+    
+                    <button type="submit"
+                            className="btn btn-outline-light">Добавить</button>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default EmpolyeesAddForm;
