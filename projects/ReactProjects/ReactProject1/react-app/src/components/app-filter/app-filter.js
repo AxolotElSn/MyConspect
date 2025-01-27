15328e1ef5428e0028bbb74d113428f08c41aaf6
Alex Orlov <axolotsn@gmail.com>
2025-01-27 14:13:47 +0400

    стилизация компонентов

--- projects/ReactProjects/ReactProject1/react-app/src/components/app-filter/app-filter.js
+++ projects/ReactProjects/ReactProject1/react-app/src/components/app-filter/app-filter.js
@@ -2,21 +2,24 @@ import './app-filter.css';
 
 const AppFilter = (props) => {
     const btnData = [ /* мы все кнопкт положили в массив для удобства */
-        {name: 'all', label: 'Все сотрудники'},
-        {name: 'star', label: 'На повышение'},
-        {name: 'moreThen1000', label: 'З/П больше 1000$'}
+        {name: 'all', label: 'Все сотрудники', colored: false},
+        {name: 'star', label: 'На повышение', colored: false},
+        {name: 'moreThen1000', label: 'З/П больше 1000$', colored: false}
     ];
 
-    const buttons = btnData.map(({name, label}) => {
+    const buttons = btnData.map(({name, label, colored}) => {
         const active = props.filter === name;
         const clazz = active ? 'btn-light' : 'btn-outline-light' /* если кнопка активная то выделяем, иначе не выделяем */
+        const style = colored ? {color: 'red'} : null /* то есть если colored true то мы возвращаем объект со стилями которые хотим накинуть на элемент. Иначе просто возвращаем null, то есть оставляем все как есть */
 
         return (
             <button
                 className={`btn ${clazz}`} /* так мы добиваемся что активная кнопка ваделяется */
                 type="button"
                 key={name}
-                onClick={() => props.onFilterSelect(name)}>
+                onClick={() => props.onFilterSelect(name)}
+                style={style} 
+                >
                 {label}
             </button>
         )
