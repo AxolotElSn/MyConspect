fb1bc638b1ebfc5bebc8749aeb278b07bf473d30
Alex <axolotsn@gmail.com>
2025-02-05 16:50:54 +0400

    трансформация данных и компонент с рандомом

--- projects/ReactProjects/ReactProject2/my-app/src/index.js
+++ projects/ReactProjects/ReactProject2/my-app/src/index.js
@@ -1,12 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./components/app/App";
-import MarvelService from "./services/MarverService";
 import "./style/style.scss";
 
-
-const marvelService = new MarvelService()
-
-marvelService.getAllCaracters().then(res => res.data.results.forEach(item => console.log(item.name)))
 
 const container = document.getElementById("root");
 const root = createRoot(container);
